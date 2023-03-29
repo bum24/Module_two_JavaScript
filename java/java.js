4af2721fab00600b7be37e758248fb78166064ca
bum24 <456456-07@mail.ru>
2023-03-29 16:54:55 +0700

    сделал домашку 03

--- java/java.js
+++ java/java.js
@@ -1,4 +1,47 @@
-let firstNumber = Number.parseFloat(prompt (`Введите первое число `)) ;
+let money = Number.parseFloat(prompt (`Ваш месячный доход?`)) ;
+let expenses = Number.parseFloat(prompt (`Перечислите возможные расходы за рассчитываемый 
+период через запятую`)) ;
+let amount = Number.parseFloat(prompt (`Во сколько обойдуться обязательные статьи расходов?`)) 
+let deposit = (confirm (`Есть ли у вас вклад в банке?`)) ;
+let purpose = Number.parseFloat(prompt (`Сколько хотите накопить денег?`)) ;
+
+let budgetDay = money-amount ;
+console.log( `${ Math.floor(budgetDay )} ₽ бюджет на месяц `);
+
+purpose = Math.ceil( purpose / budgetDay);
+console.log ( `За ${purpose} месяцев будет достигнута цель накопления`)
+
+if (budgetDay > 6000) {
+    console.log('У вас высокий уровень дохода');}
+   else if (budgetDay < 3000 > 6000 ) { 
+    console.log('У вас средний уровень дохода');} 
+   else if (budgetDay <3000 ==0) { 
+    console.log('К сожалению у вас уровень дохода ниже среднего');} 
+   else if (budgetDay <0 ) { 
+    console.log('Что то пошло не так');}
+    
+console.log('----------');
+
+
+/*const money = 1000 ;  const profit = 'фриланс' ;
+const expenses = 'Аренда, Питание, Транспорт, Досуг' ;
+const purpose = 50000 ;
+const period = 3 ;
+
+console.log( typeof money, typeof profit);
+
+console.log( expenses.length);
+
+console.log( `Период равен ${period}  месяцев`);
+console.log( `Цельзаработать ${purpose} рублей/долларов`);
+
+const budgetDay =  money / 30 ;
+console.log( `${ Math.floor(budgetDay )} ₽ / день `);*/
+
+
+
+
+/*let firstNumber = Number.parseFloat(prompt (`Введите первое число `)) ;
 let secondNumber = Number.parseFloat(prompt (`Введите второе число`) ) ;
 let a,b,c,d,i ;
 
@@ -18,29 +61,4 @@ d=Math.round(firstNumber / secondNumber);
 document.write(`Сумма деления равна ${d} <br>`)
 
 i=Math.round(firstNumber % secondNumber);
-document.write(`Сумма остатка от деления равна ${i} <br>`)
-
-
-/*
-const operation=prompt(`Введите операцию`) ;
-
-switch (operation) {
-    case`+`:
-    alert (firstNumber + secondNumber) ;
-    break;
-    case `-`:
-    alert (firstNumber - secondNumber)
-    break ;
-    case `*`:
-        alert (firstNumber * secondNumber) ;
-    break;
-    case `/`:
-        const result=(firstNumber / secondNumber)
-        alert (result.toFixed(1));
-    break;
-    case`%`:
-    alert (firstNumber % secondNumber) ;
-    break;
-
-    default: alert (`Я не знаю что вы хотите от меня!`)
-}*/
+document.write(`Сумма остатка от деления равна ${i} <br>`)*/
