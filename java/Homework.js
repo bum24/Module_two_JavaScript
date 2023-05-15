194e5f1401c9759a06548891c3025188d64c3093
bum24 <456456-07@mail.ru>
2023-05-16 00:09:01 +0700

    сделал 10 домашнюю  работу

--- java/Homework.js
+++ java/Homework.js
@@ -1,9 +1,49 @@
-// д/з 8
+// д/з 10
+let getUsersList = () => {
+    const link = 'https://reqres.in/api/users?per_page=12';
+    return fetch(link)
+        .then(response => response.json())
+}
+
+const printSurnames = (users) => {
+    users.forEach(({last_name: surname}) => console.log(surname))
+    return users
+}
+
+const printSurnamesWithF = (users) => {
+    users.filter(({last_name: surname}) => surname.startsWith('F'))
+        .forEach(user => console.log(user))
+
+    return users    
+}
+
+const printAllDatabase = (users) => {
+    const result = users.map(({first_name: name, last_name: surname}) => `${surname} ${name}`)
+        .join(', ')
+
+    console.log(`Наша база содержит данные следующих пользователей: ${result}`) 
+    
+    return users[0]
+}
+
+const printKeys = (users) => {
+    Object.keys(users).forEach(key => console.log(key))
+
+}
+
+getUsersList()
+    .then(({data:users}) => users)
+    .then(printSurnames)
+    .then(printSurnamesWithF)
+    .then(printAllDatabase)
+    .then(printKeys)
+
+/*  // д/з 8 
 let money,
     purpose,
     extraMoney,
     amount;
-    
+
 const hasDeposit = true;
 
 money = +prompt('Ваш месячный доход?');
@@ -57,8 +97,8 @@ if (budgetDay < 0 ) {
     console.log('К сожалению у вас уровень дохода ниже среднего');
 } else {
     console.log('У вас средний уровень дохода');
-}
-
+} 
+*/
 
 /* //д/з 7
 const salaries={
